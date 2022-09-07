e76d3c93f5d5bf40c8ffa5a97440b9e3536699f6
MaapTom <marcoantoniopereira41@gmail.com>
2022-09-07 10:49:24 -0300

    Add initial notes
    Adicionada anotações iniciais quando App.vue for invocado. Vue-cookie foi utilizado para registrar a sua inserção

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
 <script>
+  import VueCookie from 'vue-cookie';
+
   export default {
     data() {
       return {}
+    },
+    created() {
+      const initialNotesIsSetted = VueCookie.get('initialNotes');
+      if (initialNotesIsSetted !== 'Yes')
+        this.setInitialNotes();
+    },
+    methods: {
+      setInitialNotes() {
+        const storageNotes = localStorage.getItem('#_content-notes_#');
+        let newDateToExpires = new Date(2090,0,1);
+        let initialStorage = [
+          { 
+            id: 1,
+            titleNote: 'Criando uma anotação',
+            textNote: 'Clique em +, no canto inferior direito para criar uma anotação!',
+            date: {"date": 6, "month": 8, "year": 2022, "hour": 19, "minutes": 5}
+          },
+          { 
+            id: 10,
+            titleNote: '',
+            textNote: 'Experimente um novo estilo de layout das notas, alternando entre lista e grade no menu!',
+            date: {"date": 6, "month": 8, "year": 2022, "hour":19, "minutes": 5}
+          },
+          { 
+            id: 11,
+            titleNote: 'Dica:',
+            textNote: 'Experimente no modo "Design responsivo do navegador", :3',
+            date: {"date": 6, "month": 8, "year": 2022, "hour":19, "minutes": 5}
+          },
+          ...storageNotes || []
+        ];
+
+        localStorage.setItem('#_content-notes_#', JSON.stringify(initialStorage));
+        VueCookie.set('initialNotes', 'Yes', newDateToExpires.toGMTString());
+      }
     }
   }
 </script>
