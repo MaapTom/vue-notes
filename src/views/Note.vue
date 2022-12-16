c8c84148c34b0e2c6652c314d5b6c83ac895ca22
Marco Antonio <marcoantoniopereira41@gmail.com>
2022-12-16 12:16:34 +0000

    Update 3 files

--- src/views/Note.vue
+++ src/views/Note.vue
@@ -1,92 +1,106 @@
 <script>
-  import { setChangeNote, setDeleteNote, getNewStorage } from '../store/actions.js';
+import {
+  setChangeNote,
+  setDeleteNote,
+  getNewStorage,
+} from "../store/actions.js";
 
-  import Menu from '../components/icons/Menu.vue';
-  import Check from '../components/icons/Check.vue';
-  import LeftArrow from '../components/icons/LeftArrow.vue';
-  
-  import TextInput from '../components/TextInput.vue';
-  import TitleInput from '../components/TitleInput.vue';
-  import NoteHeader from '../components/NoteHeader.vue';
-  import ContainerModal from '../components/ContainerModal.vue';
+import Menu from "../components/icons/Menu.vue";
+import Check from "../components/icons/Check.vue";
+import LeftArrow from "../components/icons/LeftArrow.vue";
 
-  export default {
-    data() {
-      return {
-        textNote: '',
-        titleNote: '',
-        currentNote: '',
-        paramsNoteId: '',
-        toggleModal: false,
-        isActiveBtnConfirm: false,
-      }
-    },
-    components: {
-      Menu,
-      Check,
-      LeftArrow,
-      TextInput,
-      TitleInput,
-      NoteHeader,
-      ContainerModal,
-    },
-    beforeMount() {
-      this.paramsNoteId = this.$route.params.id;
+import TextInput from "../components/TextInput.vue";
+import TitleInput from "../components/TitleInput.vue";
+import NoteHeader from "../components/NoteHeader.vue";
+import ContainerModal from "../components/ContainerModal.vue";
 
-      const getStorageNotes = getNewStorage("#_content-notes_#");
-      const currentNote = getStorageNotes.filter((eachNote) => {
-        return eachNote.id == this.paramsNoteId;
-      })
+export default {
+  data() {
+    return {
+      textNote: "",
+      titleNote: "",
+      currentNote: "",
+      paramsNoteId: "",
+      toggleModal: false,
+      isActiveBtnConfirm: false,
+    };
+  },
+  components: {
+    Menu,
+    Check,
+    LeftArrow,
+    TextInput,
+    TitleInput,
+    NoteHeader,
+    ContainerModal,
+  },
+  beforeMount() {
+    this.paramsNoteId = this.$route.params.id;
 
-      if (currentNote == '') {
-        return this.$router.push('/not-found');
-      }
-      
-      this.currentNote = currentNote[0];
-    },
-    mounted() {   
-      this.textNote = this.currentNote.textNote;
-      this.titleNote = this.currentNote.titleNote;
+    const getStorageNotes = getNewStorage("#_content-notes_#");
+    const currentNote = getStorageNotes.filter((eachNote) => {
+      return eachNote.id == this.paramsNoteId;
+    });
 
-      this.$watch($data => [$data.titleNote, $data.textNote], () => {
-        if (this.titleNote != this.currentNote.titleNote || this.currentNote.textNote != '') {
+    if (currentNote == "") {
+      return this.$router.push("/not-found");
+    }
+
+    this.currentNote = currentNote[0];
+  },
+  mounted() {
+    this.textNote = this.currentNote.textNote;
+    this.titleNote = this.currentNote.titleNote;
+
+    this.$watch(
+      ($data) => [$data.titleNote, $data.textNote],
+      () => {
+        if (
+          this.titleNote != this.currentNote.titleNote ||
+          this.currentNote.textNote != ""
+        ) {
           this.isActiveBtnConfirm = true;
         } else {
           this.isActiveBtnConfirm = false;
         }
-      })
+      }
+    );
+  },
+  methods: {
+    goHome() {
+      setTimeout(() => {
+        this.$router.push("/");
+      }, 0);
     },
-    methods: {
-      goHome() {
-        setTimeout(() => {
-          this.$router.push('/');
-        }, 0)
-      },
-      setToggleModal() {
-        this.toggleModal = !this.toggleModal;
-      },
-      setTitleInput(content) {
-        this.titleNote = content;
-      },
-      setTextNote(content) {
-        this.textNote = content;
-      },
-      handleChangeNote() {
-        const response = setChangeNote(this.paramsNoteId, this.titleNote, this.textNote);
+    setToggleModal() {
+      this.toggleModal = !this.toggleModal;
+    },
+    setTitleInput(content) {
+      this.titleNote = content;
+    },
+    setTextNote(content) {
+      this.textNote = content;
+    },
+    handleChangeNote() {
+      const response = setChangeNote(
+        this.paramsNoteId,
+        this.titleNote,
+        this.textNote
+      );
 
-        if (response.status) {
-          this.isActiveBtnConfirm = false;
-        }
-      },
-      handleDeleteNote() {
-        const response = setDeleteNote(this.paramsNoteId);
-        
-        if (response.status) {
-          this.goHome();
-        }
-      },
-    }
-  }
+      if (response.status) {
+        this.isActiveBtnConfirm = false;
+      }
+    },
+    handleDeleteNote() {
+      const response = setDeleteNote(this.paramsNoteId);
+
+      if (response.status) {
+        this.goHome();
+      }
+    },
+  },
+};
 </script>
 
 <template>
@@ -103,16 +117,10 @@
         :content="titleNote"
         @change-content-title-box="setTitleInput"
       />
-      <TextInput
-        :content="textNote"
-        @change-content-text-box="setTextNote"
-      />
+      <TextInput :content="textNote" @isChanged="setTextNote" />
     </main>
 
-    <ContainerModal
-      :isActive="toggleModal"
-      @handleToggleModal="setToggleModal"
-    >
+    <ContainerModal :isActive="toggleModal" @handleToggleModal="setToggleModal">
       <ul class="container-menu">
         <li><a>Mover para</a></li>
         <li @click="handleDeleteNote"><a>Excluir</a></li>
@@ -122,7 +130,7 @@
 </template>
 
 <style scoped>
-@import '../assets/base.css';
+@import "../assets/base.css";
 
 div {
   position: absolute;
@@ -132,7 +140,7 @@ div {
 
   overflow: scroll;
   background-color: var(--color-base);
-  scrollbar-color: transparent transparent;  
+  scrollbar-color: transparent transparent;
 }
 
 .inputs-container {
@@ -156,13 +164,13 @@ div {
   position: absolute;
   top: 35px;
   right: calc(100% - 95%);
-  
+
   background: var(--color-background-light);
   border-radius: 10px;
   overflow: hidden;
 }
 
-.container-menu li a{
+.container-menu li a {
   display: block;
 
   padding: 20px 24px 16px 24px;
@@ -172,12 +180,12 @@ div {
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 }
 
-.container-menu li:last-child a{
+.container-menu li:last-child a {
   padding: 16px 24px 20px 24px;
 }
 
 .container-menu li:hover {
-  transition: all .2s;
+  transition: all 0.2s;
   background: rgba(0, 0, 0, 0.2);
 }
 </style>
